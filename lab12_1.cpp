9b569b0b607b0a3145e63e22cf354355efc84779
jirattikan-pk <mindmook.2006@gmail.com>
2025-12-30 02:57:29 -0600

    cp12-1

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,9 +1,23 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int x);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+
+int fibonacci(int x){
+    int y=0;
+    if(x<0) return y;
+    else if(x==0) return 0;
+    else if (x==1) return 1;
+    else {
+        y = fibonacci(x-1) + fibonacci(x-2);
+        return y;
+    }
+    
 }
